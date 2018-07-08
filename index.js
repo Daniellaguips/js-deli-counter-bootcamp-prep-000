18e7c9b0d7a07d450f6e2536d83b6729385018af
daniellaguips@gmail.com Guimaraes <daniellaguips@gmail.com>
2018-07-08 17:29:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(line){
  for (let i = 0; i < line.length; i++){
     line.splice(i, 1 , i+1 + ". " + line[i])
   }
-  return "The line is currently: " line
+  return "The line is currently: " +  line
 }
 
 
