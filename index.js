97012eb197246291173d384b1456842284efc437
daniellaguips@gmail.com Guimaraes <daniellaguips@gmail.com>
2018-07-08 17:21:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
+
 var katzDeli = []
 
 function takeANumber(katzDeliLine, name){
-  return "Welcome, " + name + ". You are number" + katzDeliLine + "in line."
+  katzDeliLine.push (name)
+   return "Welcome, " + katzDeliLine[katzDeliLine.length-1] + ". You are number " + katzDeliLine.length + " in line."
 }
+
+function nowServing(katzDeli){
+  if (katzDeli.length == 0){
+    return "There is nobody waiting to be served!"
+  } else return "Currently serving " + katzDeli.shift() + "."
+}
+
+function currentLine(line){
+  for (let i = 0; i < line.length; i++){
+    line.splice(i, 0, i)
+  }
+  return line
+}
+
+
+
+
+// function currentLine(line){
+// var lineNum = []
+// if (line.length == 0){
+//    return "The line is currently empty."
+//  } 
+// for (var i = 0; i <= line.length; i++ ){
+//      lineNum.push(`${i+1}. ${line[i]},`)
+//    return "The line is currently: " + lineNum
+// }
+// } 
